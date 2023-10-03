34df28adfb26e30d3891b94d34d8d6a43d3be8ee
leobewater <mmbee888@gmail.com>
2023-10-03 13:00:58 -0400

    add placeholder

--- src/components/Input/Input.stories.ts
+++ src/components/Input/Input.stories.ts
@@ -21,17 +21,20 @@ type Story = StoryObj<typeof meta>;
 export const Small: Story = {
   args: {
     size: 'small',
+    placeholder: 'Small Text',
   },
 };
 
 export const Medium: Story = {
   args: {
     size: 'medium',
+    placeholder: 'Medium Text',
   },
 };
 
 export const Large: Story = {
   args: {
     size: 'large',
+    placeholder: 'Large Text',
   },
 };
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,11 +2,14 @@ import './Input.css';
 
 interface InputProps {
   size: 'small' | 'medium' | 'large';
+  placeholder?: string;
 }
 
 const Input = (props: InputProps) => {
-  const { size = 'medium', ...rest } = props;
-  return <input className={`input ${size}`} {...rest} />;
+  const { size = 'medium', placeholder, ...rest } = props;
+  return (
+    <input className={`input ${size}`} {...rest} placeholder={placeholder} />
+  );
 };
 
 export default Input;
